6181ad76b888122ecd35c8301e628c96b44d178f
alfredo06 <alfredojosedominguezhernandez@gmail.com>
2026-06-08 21:18:11 -0500

    fix: update API_URL to use the correct production endpoint

--- frontend/src/shared/services/api.js
+++ frontend/src/shared/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import toast from "react-hot-toast";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://tracking-vehicle-aws-api.alfredo-dominguez.dev/api";
 
 const api = axios.create({
   baseURL: API_URL,
